78d17b49b9bc5895bbb26794f356cf9e39988c80
Ahmed Mustafa <am1804067@gmail.com>
2026-05-15 22:50:42 +0300

    Simple preview system

---
+++ src/routes/preview/+page.svelte
@@ -0,0 +1,21 @@
+<h1 class="font-bold text-2xl">Hi Shadowkey!</h1>
+<p>Here you can preview some pages and validate everything about the UI/UX</p>
+
+<section class="mt-6">
+    <h2 class="text-xl font-semibold">Pages</h2>
+    <ul class="mt-2">
+        <li><a href="/preview/user-profile">User Profile</a></li>
+    </ul>
+</section>
+
+<style>
+    ul {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    li {
+        border-bottom: 1px solid #333;
+    }
+</style>
