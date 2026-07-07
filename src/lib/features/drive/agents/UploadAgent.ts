1dc134ebef558ef82752448388df9f0dca074b17
Ahmed Mustafa <am1804067@gmail.com>
2026-07-07 14:12:44 +0300

    UploadAgent Skeleton

--- src/lib/features/drive/agents/UploadAgent.ts
+++ src/lib/features/drive/agents/UploadAgent.ts
@@ -1,3 +1,38 @@
+import type { AxiosInstance } from "axios"
+
 export default class UploadAgent {
     
+    private api: AxiosInstance;
+
+    constructor(api: AxiosInstance) {
+        this.api = api;
+    }
+
+    public uploadFile(src: string, dest: string) {
+        throw new Error("Method not implemented.");
+    }
+
+    private checkRepositoryAvailability() : boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    private createRepository() : boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    private ghostStage(repoPath: string, filepath: string) {
+        throw new Error("Method not implemented.");
+    }
+
+    private switchBranch(repoPath: string, branch: string) {
+        throw new Error("Method not implemented.");
+    }
+
+    private commit(repoPath: string) {
+        throw new Error("Method not implemented.");
+    }
+
+    private push(repoPath: string, branch: string) {
+        throw new Error("Method not implemented.");
+    }
 }
