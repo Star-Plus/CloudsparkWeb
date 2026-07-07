37fac10ace60316b4dec2a0184154bd92139a0db
Ahmed Mustafa <am1804067@gmail.com>
2026-07-07 16:03:21 +0300

    feat: Enhance UploadAgent with WebSocket support and file upload functionality
    - Added WebSocket integration to UploadAgent for real-time communication.
    - Implemented methods for checking repository availability, creating repositories, ghost staging, switching branches, committing, and pushing files.
    - Updated constructor to accept a WebSocket URL and handle socket events.
    - Created a new test file for UploadAgent.
    - Added Jest as a development dependency in package.json.

--- src/lib/features/drive/agents/UploadAgent.ts
+++ src/lib/features/drive/agents/UploadAgent.ts
@@ -3,36 +3,90 @@ import type { AxiosInstance } from "axios"
 export default class UploadAgent {
     
     private api: AxiosInstance;
+    private socket: WebSocket;
 
-    constructor(api: AxiosInstance) {
+    constructor(api: AxiosInstance, socketUrl: string) {
         this.api = api;
+        this.socket = new WebSocket(socketUrl);
+
+        this.socket.onopen = this.handleOnSocketOpen.bind(this);
+        this.socket.onclose = this.handleOnClose.bind(this);
+        this.socket.onerror = this.handleOnError.bind(this);
+        this.socket.onmessage = this.handleOnMessage.bind(this);
     }
 
-    public uploadFile(src: string, dest: string) {
-        throw new Error("Method not implemented.");
+    public async uploadFile(src: string, dest: string) {
+        try {
+            const repoPath = dest.split("/").slice(0, 2).join("/");
+            
+            if (!await this.checkRepositoryAvailability(repoPath)) {
+                await this.createRepository(repoPath);
+            }
+            
+            await this.ghostStage(src, dest);
+            await this.switchBranch(src, dest);
+            await this.commit(src);
+            await this.push(src, dest);
+        }
+        catch (err) {
+            console.error(err);
+        }
     }
 
-    private checkRepositoryAvailability() : boolean {
-        throw new Error("Method not implemented.");
+    private async checkRepositoryAvailability(repoPath: string) : Promise<boolean> {
+        const resp = await this.api.get(`/repo/${repoPath}/exists`);
+        if (resp.status == 404) return false;
+        if (resp.status !== 200) throw new Error(resp.data);
+
+        return true;
     }
 
-    private createRepository() : boolean {
-        throw new Error("Method not implemented.");
+    private async createRepository(repoPath: string) {
+        const resp = await this.api.post(`/initialize/repository/appData/repos/${repoPath}`);
+        if (resp.status !== 200) throw new Error(resp.data);
     }
 
-    private ghostStage(repoPath: string, filepath: string) {
-        throw new Error("Method not implemented.");
+    private async ghostStage(repoPath: string, filepath: string) {
+        const resp = await this.api.post(`/${repoPath}/ghostStage?filepath=${filepath}`);
+        if (resp.status !== 200) throw new Error(resp.data);
     }
 
-    private switchBranch(repoPath: string, branch: string) {
-        throw new Error("Method not implemented.");
+    private async switchBranch(repoPath: string, branch: string) {
+        const resp = await this.api.post(`/${repoPath}/switch?branch=${branch}`);
+        if (resp.status !== 200) throw new Error(resp.data);
     }
 
-    private commit(repoPath: string) {
-        throw new Error("Method not implemented.");
+    private async commit(repoPath: string) {
+        const resp = await this.api.post(`/${repoPath}/commit`);
+        if (resp.status !== 200) throw new Error(resp.data);
     }
 
-    private push(repoPath: string, branch: string) {
-        throw new Error("Method not implemented.");
+    private async push(repoPath: string, branch: string) {
+        const resp = await this.api.post(`/${repoPath}/push/${branch}`);
+        if (resp.status !== 200) throw new Error(resp.data);
+    }
+
+    private handleOnSocketOpen(event: Event) {
+        console.log('Socket opened');
+        const payload = JSON.stringify({action: 'greet', message: 'Hello from the client!'});
+        this.socket.send(payload);
+    }
+
+    private handleOnMessage(event: MessageEvent) {
+        try {
+            const data = JSON.parse(event.data);
+            console.log("Mesasge received from server", data);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    private handleOnError(event: Event) {
+        console.error("WebSocket error observed:", event);
+    }
+
+    private handleOnClose(event: CloseEvent) {
+        console.log("WebSocket closed", event);
     }
 }
