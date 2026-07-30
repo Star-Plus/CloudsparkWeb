31abfa50caa0a55ed95486f35d5274049e42e111
Ahmed Mustafa <am1804067@gmail.com>
2026-07-31 02:35:16 +0300

    Give the agent login tools

--- src/lib/features/drive/agents/download/DownloadAgent.ts
+++ src/lib/features/drive/agents/download/DownloadAgent.ts
@@ -1,3 +1,4 @@
+import type AuthService from "$lib/features/auth/AuthService";
 import BoxTask from "$lib/features/tasks/BoxTask.svelte";
 import ConfirmableTask from "$lib/features/tasks/ConfirmableTask.svelte";
 import ProgressingTask from "$lib/features/tasks/ProgressingTask.svelte";
@@ -7,11 +8,13 @@ import type { AxiosInstance } from "axios";
 export default class DownloadAgent {
 
     private api: AxiosInstance;
+    private authService: AuthService;
     private socketBaseUrl?: string;
     private vcsUrl?: string;
 
-    constructor(api: AxiosInstance, socketUrl?: string, vcsUrl?: string) {
+    constructor(api: AxiosInstance, authService: AuthService, socketUrl?: string, vcsUrl?: string) {
         this.api = api;
+        this.authService = authService;
         this.socketBaseUrl = socketUrl;
         this.vcsUrl = vcsUrl;
     }
@@ -34,6 +37,7 @@ export default class DownloadAgent {
             const repoPath = filepath.split("/").slice(0, 2).join("/");
 
             const remoteUrl = this.vcsUrl + "/api/" + repoPath;
+            await this.login();
             await this.createRepository(repoPath, remoteUrl);
 
             const relativePath = filepath.split("/").slice(2).filter(p => p !== "").join("/");
@@ -140,6 +144,14 @@ export default class DownloadAgent {
         if (resp.status !== 200) throw new Error(resp.data);
     }
 
+    private login() {
+        if (!this.authService.isAuthenticated()) throw new Error("Not logged in");
+        const user = this.authService.getUser();
+
+        return this.api.post("/login", null, {
+            params: { username: user?.username, token: user?.token}
+        });
+    }
     
     private async switchBranch(repoPath: string, branch: string) {
         const owner = repoPath.split("/")[0];
